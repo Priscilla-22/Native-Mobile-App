f747af1a85093b81341ce7685dad1d6433f5cca8
Priscilla-22 <priscillamw123@gmail.com>
2024-05-05 19:24:20 +0300

    feat: load fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Slot,Stack} from 'expo-router';
-import{useFonts} from 'expo-font'
+import { useFonts } from 'expo-font'
+
 const RootLayout = () => {
+  const [FOntsLoaded,error] = use
   return (
   <Stack>
     <Stack.Screen name="index" options={{headerShown: false}} />
