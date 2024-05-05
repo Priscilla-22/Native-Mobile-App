6336ccd53c401639a865dfc41e9b063389d159c4
Priscilla-22 <priscillamw123@gmail.com>
2024-05-05 20:10:07 +0300

    chore: File and folder structure setup

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 const RootLayout = () => {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl'> Root Layout</Text>
+      <Text className='text-3xl font-pblack'> Aora! </Text>
       <StatusBar style='auto' />
       <Link href='/profile' style={{ color: 'blue' }}>
         Go to Profile
