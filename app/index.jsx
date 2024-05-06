6071e0ca87ae4145afe8cfd3e13b6ef5dea5ca34
Priscilla-22 <priscillamw123@gmail.com>
2024-05-06 09:30:38 +0300

    feat: handle press for the btn to redirect the user to auth

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import { Image, ScrollView, Text, View } from 'react-native';
 import React from 'react';
-import { Link, link } from 'expo-router';
+import { Link,Redirect,router  } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 const RootLayout = () => {
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView contentCOntainerStyle={{ height: '100%' }}>
-        <View className='w-full justify-center items-center h-full px-4'>
+        <View className='w-full justify-center items-center min-h-[85vh] px-4'>
           <Image
             source={images.logo}
             className='w-[130px] h-[84px]'
@@ -34,8 +35,14 @@ const RootLayout = () => {
             />
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration</Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light'/>
     </SafeAreaView>
   );
 };
